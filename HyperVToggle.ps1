954803c5b9b7ea6dbb101944aeb457160ec82ff0
Andrea Robertsen <40151168+khanichan@users.noreply.github.com>
2023-05-03 20:47:24 +0200

    Update HyperVToggle.ps1
    Changed some text outputs since I forgot to edit them when copying lines.
    
    Added read-host at bottom so its possible to read the output before the script closes.

--- HyperVToggle.ps1
+++ HyperVToggle.ps1
@@ -18,18 +18,21 @@ elseif ($action -eq "AddFeature") {
     #Enable Hyper-V
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
     Write-Host "Hyper-V Feature has been Added"
+    Read-Host "Press Enter to exit"
 }
 elseif ($action -eq "RemoveFeature") {
     #Disable Hyper-V
     Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
     Write-Host "Hyper-V has Feature been Removed"
-}
-elseif ($action -eq "help") {
+    Read-Host -Prompt "Press any key to continue. . ."}
+elseif (($action -eq "help") -or ($action -eq "Help") -or ($action -eq "HELP") ) {
     Write-Host "Enable - Enables Hyper-V after disabling it previously with bcdedit and registry command."
     Write-Host "Disable - Disables Hyper-V with a bcdedit with registry command (bcdedit, reg add)"
-    Write-Host "AddFeature - Enables Hyper-V with Enable-WindowsOptionalFeature"
-    Write-Host "RemoveFeature - Enables Hyper-V with Disable-WindowsOptionalFeature"
+    Write-Host "AddFeature - Adds Hyper-V Feature with Enable-WindowsOptionalFeature"
+    Write-Host "RemoveFeature - Removes Hyper-V Feature with Disable-WindowsOptionalFeature"
 }
 else {
     Write-Host "Invalid input. Please type Enable, Disable, AddFeature or RemoveFeature"
 }
+Read-Host -Prompt "Press any key to exit. . ."
+exit
